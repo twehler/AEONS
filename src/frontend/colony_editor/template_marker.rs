34fd76bbcdf3c617906d258b48103643c697705b
Thomas Wehler <thomas.wehler@eclipso.de>
2026-06-23 13:38:02 +0200

    Delete src directory

--- src/frontend/colony_editor/template_marker.rs
+++
@@ -1,7 +0,0 @@
-// Marker on a placed template's visual mesh, carrying the template id.
-
-use bevy::prelude::*;
-
-#[derive(Component)]
-#[allow(dead_code)] // Reserved for future "click visual to select template" UX.
-pub struct EditorTemplateMarker(pub u32);
